53718d9f4bccd347267fc9bc76c737ff76cfca95
Crip Zone <9021853+Crip-Zone@users.noreply.github.com>
2025-10-19 20:11:27 -0400

    Update ThemeStrings.english.php

--- languages/ThemeStrings.english.php
+++ languages/ThemeStrings.english.php
@@ -1,6 +1,7 @@
 <?php
 $txt['Navigate'] = 'Navigate';
-$txt['Design'] = 'Design';
+$txt['Design'] = 'Designr';
 $txt['copyrights'] = 'Copyrights';
-$txt['themecopyright'] = 'Test by <a href="https://www.jpr62.com/theme/" target="_blank" class="new_win" title="Crip Zone">Crip</a> <br> Updated for SMF 2.1<br>By <a href="https://www.jpr62.com/theme/" target="_blank" class="new_win" title="TheCripZone">TheCripZone</a>';
+$txt['themecopyright'] = 'Test by <a href="https://www.jpr62.com/theme/" target="_blank" class="new_win" title="Crip Zone">Crip</a> <br> Updated for SMF 2.1.6<br>By <a href="https://www.jpr62.com/theme/" target="_blank" class="new_win" title="TheCripZone">TheCripZone</a>';
 ?>
+
